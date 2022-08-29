237b64b57c8f34b6af09c3587602aed9ccf96308
Ramon Marquez <rmarquez@rmarquez.rmarquez>
2022-08-28 20:13:21 -0400

    Added heroku settings

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn landing_page.wsgi:application --log-file --log-level debug
+manage.py migrate
--- landing_page/settings.py
+++ landing_page/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = os.environ.get('STATIC_URL')
-# STATIC_ROOT = os.environ.get('STATIC_ROOT')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = os.environ.get('MEDIA_URL')
